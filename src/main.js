b085c5dc84ede1adc0ae7b37ae459e98d46838f5
Hiren Keraliya <hiren@freshbits.in>
2020-01-09 17:12:53 +0530

    Add vue routing and manage it

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import VueRouter from 'vue-router'
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
